79d1dc2b601a2e3c08c61ba4c95aaa2c91386e45
Erik Maulana <muherik.maulana@gmail.com>
2019-01-15 23:40:44 -0800

    Ubah

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,3 @@
 $(document).ready(function(){
-  $('.slider').owlCarousel({
-              items: 1,
-              singleItem:true,
-              nav: true,
-              dots: false,
-              loop: true,
-              autoPlay: 3000
-          });
+
 });
