f5fc4e27b63dfa89d4de5e6108ccd59868750942
Erik Maulana <muherik.maulana@gmail.com>
2019-01-16 20:45:41 -0800

    update Navbar

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
+
   jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 100  // 2 seconds
+
+  // fade in .navbar
+  $(function () {
+    $(window).scroll(function () {
+       // set distance user needs to scroll before we start fadeIn
+      if ($(this).scrollTop() > 100) {
+        $('.navbar').addClass("bg-second");
+        $('.navbar').removeClass("bg-first");
+        $('#img-logo').attr("src","assets/images/logo 4.png");
+      } else {
+        $('.navbar').removeClass("bg-second");
+        $('.navbar').addClass("bg-first");
+        $('#img-logo').attr("src","assets/images/logo 2.png");
+      }
+    });
+  });
+
 });
